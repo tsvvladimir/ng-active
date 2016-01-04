2b2ecc0827f10ef68656907948f682519668a00b
Vladimir Tsvetkov <tsvvladimir95@gmail.com>
2016-01-04 22:51:31 +0300

    changed output

--- proj/main.py
+++ proj/main.py
@@ -29,10 +29,13 @@ text_clf = Pipeline([
 
 text_clf.fit(twenty_train_data, twenty_train_target)
 predicted = text_clf.predict(twenty_test_data)
-print "baseline f1 micro", f1_score(twenty_test_target, predicted, average='micro')
+cur_score = f1_score(twenty_test_target, predicted, average='micro')
+print "baseline"
+print "(", len(twenty_test_target), ", ", cur_score, ")"
 
 
 #add random elements strategy
+print "random sampling solution"
 alpha = 100 #initial training set
 betha = 100 #number of iteration
 gamma = 50 #number of sampling
@@ -45,39 +48,25 @@ twenty_unlabeled_target = twenty_train_target[alpha:]
 text_clf.fit(twenty_cur_training_data, twenty_cur_training_target)
 predicted = text_clf.predict(twenty_test_data)
 cur_score = f1_score(twenty_test_target, predicted, average='micro')
-print "train set", len(twenty_cur_training_data), "cur_score", cur_score
+print "(", len(twenty_cur_training_data), ", ", cur_score, ")"
 
 for t in range(1, betha):
     sample_numbers = randint(0, len(twenty_unlabeled_data), gamma)
     sample_data = list(twenty_unlabeled_data)
     sample_target = list(twenty_unlabeled_target)
-    #print "will take this idx", sample_numbers, "while length of set is", len(twenty_unlabeled_data)
     for i in range(0, len(sample_numbers)):
-        #try:
         temp1 = twenty_unlabeled_data[sample_numbers[i]]
-            #print "add document", temp1
         temp2 = twenty_unlabeled_target[sample_numbers[i]]
-            #print "add target", temp2
         twenty_cur_training_data = np.append(twenty_cur_training_data, temp1)
-        #print "1smth with idx", i
         twenty_cur_training_target = np.append(twenty_cur_training_target, temp2)
-        #print "2smth with idx", i
         sample_data.pop(i)
-        #print "3smth with idx", i
         sample_target.pop(i)
-        #print "4smth with idx", i
-        #except:
-        #    print "Unexpected error:", sys.exc_info()[0]
     twenty_unlabeled_data = sample_data
     twenty_unlabeled_target = sample_target
 
     text_clf.fit(twenty_cur_training_data, twenty_cur_training_target)
     predicted = text_clf.predict(twenty_test_data)
-    #print "len of test data", len(twenty_test_data)
-    #print "shape of decision_function onall test data", text_clf.decision_function(twenty_test_data).shape
     cur_score = f1_score(twenty_test_target, predicted, average='micro')
-    print "train set", len(twenty_cur_training_data), "cur_score", cur_score
-    #print len(np.array([twenty_cur_training_data[0]]))
-    #print "decision_function", text_clf.decision_function(np.array([twenty_cur_training_data[0]]))
-    #print "predicted label for 0", text_clf.predict(np.array([twenty_cur_training_data[0]]))
+    print "(", len(twenty_cur_training_data), ", ", cur_score, ")"
+
 
