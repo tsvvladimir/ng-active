396188d45b83b29cdb1c2e345f9aa9a693c87288
Vladimir Tsvetkov <tsvvladimir95@gmail.com>
2016-01-04 23:58:12 +0300

    add range by least confident

--- proj/main.py
+++ proj/main.py
@@ -9,6 +9,7 @@ from sklearn.metrics import f1_score
 from numpy.random import randint
 import logging
 import sys
+import operator
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -25,6 +26,7 @@ text_clf = Pipeline([
     ('clf', LinearSVC())
 ])
 
+'''
 #baseline
 
 text_clf.fit(twenty_train_data, twenty_train_target)
@@ -34,10 +36,11 @@ print "baseline"
 print "(", len(twenty_test_target), ", ", cur_score, ")"
 
 
+
 #add random elements strategy
 print "random sampling solution"
 alpha = 100 #initial training set
-betha = 100 #number of iteration
+betha = 10 #number of iteration
 gamma = 50 #number of sampling
 
 twenty_cur_training_data = twenty_train_data[:alpha]
@@ -70,3 +73,107 @@ for t in range(1, betha):
     print "(", len(twenty_cur_training_data), ", ", cur_score, ")"
 
 
+
+#range by least confident
+print "range by least confident"
+alpha = 100 #initial training set
+betha = 10 #number of iteration
+gamma = 50 #number of sampling
+
+twenty_cur_training_data = twenty_train_data[:alpha]
+twenty_cur_training_target = twenty_train_target[:alpha]
+twenty_unlabeled_data = twenty_train_data[alpha:]
+twenty_unlabeled_target = twenty_train_target[alpha:]
+
+text_clf.fit(twenty_cur_training_data, twenty_cur_training_target)
+predicted = text_clf.predict(twenty_test_data)
+cur_score = f1_score(twenty_test_target, predicted, average='micro')
+print "(", len(twenty_cur_training_data), ", ", cur_score, ")"
+
+for t in range(1, betha):
+    #sample_numbers = randint(0, len(twenty_unlabeled_data), gamma)
+    confidence_scores = text_clf.decision_function(twenty_unlabeled_data)
+    #print len(twenty_unlabeled_data)
+    #print confidence_scores.shape
+
+    doc_score = {}
+    for i in range(0, len(twenty_unlabeled_data)):
+        doc_score[i] = np.amax(confidence_scores[i])
+
+    sorted_doc_score = sorted(doc_score.items(), key=operator.itemgetter(1))
+
+    sample_numbers = np.array([])
+    for i in range(0, gamma):
+        #print i, type(sorted_doc_score[i][0])
+        sample_numbers = np.append(sample_numbers, (sorted_doc_score[i][0]))
+
+    #sample_numbers - indices which we should add
+    sample_data = list(twenty_unlabeled_data)
+    sample_target = list(twenty_unlabeled_target)
+    for i in range(0, len(sample_numbers)):
+        #print type(sample_numbers[i])
+        temp1 = twenty_unlabeled_data[int(sample_numbers[i])]
+        temp2 = twenty_unlabeled_target[int(sample_numbers[i])]
+        twenty_cur_training_data = np.append(twenty_cur_training_data, temp1)
+        twenty_cur_training_target = np.append(twenty_cur_training_target, temp2)
+        sample_data.pop(i)
+        sample_target.pop(i)
+    twenty_unlabeled_data = sample_data
+    twenty_unlabeled_target = sample_target
+
+    text_clf.fit(twenty_cur_training_data, twenty_cur_training_target)
+    predicted = text_clf.predict(twenty_test_data)
+    cur_score = f1_score(twenty_test_target, predicted, average='micro')
+    print "(", len(twenty_cur_training_data), ", ", cur_score, ")"
+'''
+#range by minimum margin
+print "range by minimum margin"
+alpha = 100 #initial training set
+betha = 10 #number of iteration
+gamma = 50 #number of sampling
+
+twenty_cur_training_data = twenty_train_data[:alpha]
+twenty_cur_training_target = twenty_train_target[:alpha]
+twenty_unlabeled_data = twenty_train_data[alpha:]
+twenty_unlabeled_target = twenty_train_target[alpha:]
+
+text_clf.fit(twenty_cur_training_data, twenty_cur_training_target)
+predicted = text_clf.predict(twenty_test_data)
+cur_score = f1_score(twenty_test_target, predicted, average='micro')
+print "(", len(twenty_cur_training_data), ", ", cur_score, ")"
+
+for t in range(1, betha):
+    #sample_numbers = randint(0, len(twenty_unlabeled_data), gamma)
+    confidence_scores = text_clf.decision_function(twenty_unlabeled_data)
+    #print len(twenty_unlabeled_data)
+    #print confidence_scores.shape
+
+    doc_score = {}
+    for i in range(0, len(twenty_unlabeled_data)):
+        doc_score[i] = np.amax(confidence_scores[i])
+
+    sorted_doc_score = sorted(doc_score.items(), key=operator.itemgetter(1))
+
+    sample_numbers = np.array([])
+    for i in range(0, gamma):
+        #print i, type(sorted_doc_score[i][0])
+        sample_numbers = np.append(sample_numbers, (sorted_doc_score[i][0]))
+
+    #sample_numbers - indices which we should add
+    sample_data = list(twenty_unlabeled_data)
+    sample_target = list(twenty_unlabeled_target)
+    for i in range(0, len(sample_numbers)):
+        #print type(sample_numbers[i])
+        temp1 = twenty_unlabeled_data[int(sample_numbers[i])]
+        temp2 = twenty_unlabeled_target[int(sample_numbers[i])]
+        twenty_cur_training_data = np.append(twenty_cur_training_data, temp1)
+        twenty_cur_training_target = np.append(twenty_cur_training_target, temp2)
+        sample_data.pop(i)
+        sample_target.pop(i)
+    twenty_unlabeled_data = sample_data
+    twenty_unlabeled_target = sample_target
+
+    text_clf.fit(twenty_cur_training_data, twenty_cur_training_target)
+    predicted = text_clf.predict(twenty_test_data)
+    cur_score = f1_score(twenty_test_target, predicted, average='micro')
+    print "(", len(twenty_cur_training_data), ", ", cur_score, ")"
